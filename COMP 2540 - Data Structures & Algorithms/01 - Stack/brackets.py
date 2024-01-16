9a721fc1b06b55a361231956c9f71be0fc9ca5e6
Ethan Walker <81318227+EthanWalkerGit@users.noreply.github.com>
2024-01-16 18:48:24 -0500

    Add files via upload

---
+++ COMP 2540 - Data Structures & Algorithms/01 - Stack/brackets.py
@@ -0,0 +1,32 @@
+from stack import ArrayStack
+"""push, pop, top, is_empty"""
+
+S = ArrayStack()
+case = input("Enter test case: \n")
+
+for i in case:
+    if i == '(' or i == '[' or i == '{':
+        S.push(i)
+    if i == ')':
+        if S.top() == '(':
+            S.pop()
+        else:
+            print("The input is not balanced.")
+            break
+    if i == ']':
+        if S.top() == '[':
+            S.pop()
+        else:
+            print("The input is not balanced.")
+            break
+    if i == '}':
+        if S.top() == '{':
+            S.pop()
+        else:
+            print("The input is not balanced.")
+            break
+
+if S.is_empty():
+    print("The input is balanced")
+
+
